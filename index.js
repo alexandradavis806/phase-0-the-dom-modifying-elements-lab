9659d4dfdc37e6a95657b5f2501054864f91b9bd
Alexandra Davis <alex.davis806@gmail.com>
2022-03-19 21:15:27 -0700

    commit tree cfac9cf01b03d3762493e813e38834ae19d904b1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const main = document.getElementById('main')
+
+main.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = 'is the champion'
+document.append(newHeader);
+
+// h1.textContent
